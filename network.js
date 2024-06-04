19ca577279760c17bf46c148d1e733c199756747
finefin <info@finefin.com>
2024-06-04 14:49:14 +0200

    client/serv

--- network.js
+++ network.js
@@ -1,99 +1,60 @@
-/*
-HINTPAD NETWORK
--> server: using Nodejs + faye websocket to shutdown/reboot Hintpad PC
-*/
-var http = require("http"),
-url = require("url"),
-fs = require("fs"),
-port = 3015;
+// server.js
+const http = require('http');
+const faye = require('faye');
+const fs = require('fs');
+const path = require('path');
+const { reboot, shutdown } = require('node-shutdown-windows');
 
-const { spawn } = require("child_process");
-const path    = require('path');
-const mPath = path.resolve(__dirname, '..');
+// Create a basic HTTP server
+const server = http.createServer((req, res) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-var faye = require ('faye');
-var faye_server = new faye.NodeAdapter ({mount: '/faye', timeout: 120});
-var http_server = http.createServer(function(req,res){
-
-  const headers = {
-    'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
-    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-    'Access-Control-Max-Age': 2592000, // 30 days
-  };
-
-
-  // get the DATA
-  let data = '';
-  req.on('data', chunk => {
-    data += chunk;
-    console.log(`Data chunk available: ${chunk}`)
-
-    faye_server.getClient().publish('/faye',
-    {
-      action: JSON.parse(data).action
-    });
-
-  });
-  req.on('end', () => {
-    // end of data
-  })
-
-  // show the page
-  var uri = url.parse(req.url).pathname
-  , filename = path.join(process.cwd(), './index.html', uri);
-
-  fs.exists(filename, function(exists) {
-
-    if(!exists) {
-      res.writeHead(404, {"Content-Type": "text/plain"});
-      res.write("---> 404 Not Found\n");
-      res.end();
-      return;
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/faye-browser.js') {
+        fs.readFile(path.join(__dirname, 'node_modules', 'faye', 'browser', 'faye-browser.js'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/javascript' });
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
     }
-
-    // display the html containing the phaser 3 canvas
-    if (fs.statSync(filename).isDirectory()) filename += 'index.html';
-
-    fs.readFile(filename, "binary", function(err, file) {
-
-                if (err) {
-                    res.writeHeader(404, {
-                          'Content-Type': 'text/plain'
-                      })
-                      res.write('-> 404 Not Found')
-                      res.end()
-                      return
-                  }
-
-                  if (req.url.endsWith('.html')) {
-                      /*res.writeHeader(200, {
-                          'Content-Type': 'text/html'
-                      })*/
-                      res.setHeader("Content-Type", "text/html");
-                  }
-
-                  if (req.url.endsWith('.js')) {
-                      /*res.writeHeader(200, {
-                          'Content-Type': 'application/javascript',
-                      })*/
-                      res.setHeader("Content-Type", "application/javascript");
-                  }
-
-                  if (req.url.endsWith('.mjs')) {
-                      /*res.writeHeader(200, {
-                          'Content-Type': 'module',
-                      })*/
-
-                      res.setHeader("Content-Type", "application/javascript");
-                  }
-
-      res.writeHead(200);
-      res.write(file, "binary");
-      res.end();
-    });
-  });
 });
 
-faye_server.attach(http_server);
-http_server.listen(port,10);
-  "Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+// Attach Faye to the server
+const bayeux = new faye.NodeAdapter({ mount: '/faye', timeout: 45 });
+bayeux.attach(server);
+
+// Handle Faye messages
+bayeux.getClient().subscribe('/commands', (message) => {
+    if (message.action === 'reboot') {
+        console.log('Rebooting...');
+        reboot();
+    } else if (message.action === 'shutdown') {
+        console.log('Shutting down...');
+        shutdown();
+    }
+});
+
+// Start the server
+const PORT = 8000;
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
