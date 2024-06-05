ced1999751c37b4a4e3508d2de60659c5fea3ff4
finefin <info@finefin.com>
2024-06-05 09:41:36 +0200

    fixed http delivery

--- network.js
+++ network.js
@@ -12,17 +12,29 @@ const mPath = path.resolve(__dirname, '..');
 
 var faye = require ('faye');
 var faye_server = new faye.NodeAdapter ({mount: '/faye', timeout: 120});
+
+
 var http_server = http.createServer(function(req,res){
+
+  const headers = {
+    'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Max-Age': 2592000, // 30 days
+  };
+
 
   // get the DATA
   let data = '';
   req.on('data', chunk => {
     data += chunk;
-    console.log(`Data chunk available: ${JSON.parse(data).action}`)
+    console.log(`Data chunk available: ${chunk}`)
 
     if (JSON.parse(data).action == "reboot") {
-        console.log ("REBOOT!");
-        
+        console.log ("REBOOT!");    
+    }
+
+    if (JSON.parse(data).action == "shutdown") {
+        console.log ("SHUTDOWN!");    
     }
 
 
@@ -31,8 +43,64 @@ var http_server = http.createServer(function(req,res){
     // end of data
   })
 
+  // show the page
+  var uri = url.parse(req.url).pathname
+  , filename = path.join(process.cwd(), './index.html', uri);
 
+  fs.exists(filename, function(exists) {
+
+    if(!exists) {
+      res.writeHead(404, {"Content-Type": "text/plain"});
+      res.write("---> 404 Not Found\n");
+      res.end();
+      return;
+    }
+
+    // display the html containing the phaser 3 canvas
+    if (fs.statSync(filename).isDirectory()) filename += 'index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+
+                if (err) {
+                    res.writeHeader(404, {
+                          'Content-Type': 'text/plain'
+                      })
+                      res.write('-> 404 Not Found')
+                      res.end()
+                      return
+                  }
+
+                  if (req.url.endsWith('.html')) {
+                      /*res.writeHeader(200, {
+                          'Content-Type': 'text/html'
+                      })*/
+                      res.setHeader("Content-Type", "text/html");
+                  }
+
+                  if (req.url.endsWith('.js')) {
+                      /*res.writeHeader(200, {
+                          'Content-Type': 'application/javascript',
+                      })*/
+                      res.setHeader("Content-Type", "application/javascript");
+                  }
+
+                  if (req.url.endsWith('.mjs')) {
+                      /*res.writeHeader(200, {
+                          'Content-Type': 'module',
+                      })*/
+
+                      res.setHeader("Content-Type", "application/javascript");
+                  }
+
+      res.writeHead(200);
+      res.write(file, "binary");
+      res.end();
+    });
+  });
 });
+
+
+
 
 faye_server.attach(http_server);
 http_server.listen(port,10);
